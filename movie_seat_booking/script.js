02b3c9a2b7929fb75f4e3e68aa41034d1895dc74
송승찬 <superfly09@daum.net>
2022-12-18 14:57:39 +0900

    fix: localStorage에 값 저장 및 리로드 후 저장한 값 불러오는 부분 추가

--- movie_seat_booking/script.js
+++ movie_seat_booking/script.js
@@ -4,21 +4,46 @@ const $movieContainer = document.querySelector('#movie');
 const $selectedSeatCount = document.querySelector('#count');
 const $totalPrice = document.querySelector('#total');
 
-
 const saveToLocalStorage = (key, value)=> localStorage.setItem(key, JSON.stringify(value));
+const getDataFromLocalStorage = key=> JSON.parse(localStorage.getItem(key));
 
 const calculatePrice = () =>{
     const selectedSeatsCount = document.querySelectorAll('.row .seat.selected').length;
+    const moviePrice = $movieContainer.value;
     $selectedSeatCount.innerText  = selectedSeatsCount;
-    $totalPrice.innerText = selectedSeatsCount * $movieContainer.value;
+    $totalPrice.innerText = selectedSeatsCount * moviePrice;
 }
 //change발생시 selectedIndex도 같이 바뀜
-$movieContainer.addEventListener('change', calculatePrice)
+$movieContainer.addEventListener('change', (e)=>{
+    saveToLocalStorage('selectedMovie', JSON.stringify({ index: e.target.selectedIndex, price : +e.target.value }));
+    calculatePrice();
+})
 
 $container.addEventListener("click", (e) => {
   if (e.target.classList.contains("seat") && !e.target.classList.contains('occupied')) {
-    e.target.classList.toggle('selected')
+    e.target.classList.toggle('selected');
+    
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const selectedSeatsIndex = [...selectedSeats].map(seat=>[...$usableSeat].indexOf(seat));
+    
+    saveToLocalStorage('selectedSeatsIndex', JSON.stringify(selectedSeatsIndex))
     calculatePrice();
   }
 });
 // classList.toggle => Removes a given token from the list and returns false. If token doesn't exist it's added and the function returns true.
+
+const init = () =>{
+  const defaultValue = { index: 0 , price : +$movieContainer.querySelector('option').value}
+
+  const {index} = JSON.parse(getDataFromLocalStorage('selectedMovie')) ?? defaultValue
+  const selectedSeatsIndex = JSON.parse(getDataFromLocalStorage('selectedSeatsIndex'));
+
+  [...$usableSeat].forEach((seat, index)=>{
+    if(selectedSeatsIndex.indexOf(index) > -1) seat.classList.add('selected')
+  });
+
+  $movieContainer.selectedIndex = index
+  calculatePrice();
+}
+
+document.addEventListener('DOMContentLoaded', init)
