fff0e5608e2cf780ce10d877ca1dd65c6e39b473
seungchan <superfly9@encar.com>
2022-12-28 13:34:17 +0900

    feat: input통한 환율 계산 처리 추가

--- exchange_rate_calculator/script.js
+++ exchange_rate_calculator/script.js
@@ -1,9 +1,11 @@
 const qs = (target, scope = document) => scope.querySelector(target);
 
 const currencyOne = qs("#currency_one");
+const amountOne = qs('#amount_one');
 const currencyTwo = qs("#currency_two");
+const amountTwo = qs('#amount_two');
 const swapBtn = qs("#swap_btn");
-const compareResult = qs("#result");
+const rate = qs("#rate");
 
 const getCurrencyData = async (url) => {
   const res = await fetch(url);
@@ -23,12 +25,32 @@ const addCountryToSelect = (target, data = [],initialCurrency) => {
   });
 };
 
+let fromCurrency = 'USD';
+let toCurrency ='KRW';
+let fromAmount = amountOne.value;
+let toAmount = amountTwo.value;
+let currencyRate = {};
+const fixedHandler = (value, digit = 2) => value.toFixed(digit)
 
 const init = async () => {
   const currencyList = await getCurrencyData('currency.json');
-  addCountryToSelect(currencyOne, currencyList, 'KRW');
-  addCountryToSelect(currencyTwo, currencyList, 'USD');
-  const currencyRate = await getCurrencyData(`https://open.er-api.com/v6/latest/KRW`);
+  addCountryToSelect(currencyOne, currencyList, fromCurrency);
+  addCountryToSelect(currencyTwo, currencyList, toCurrency);
+  currencyRate = (await getCurrencyData(`https://open.er-api.com/v6/latest/${fromCurrency}`))['rates'];
+  console.log(currencyRate)
+  rate.innerText = `1 ${fromCurrency} ~ ${currencyRate[toCurrency].toFixed(2)} ${toCurrency}`;
 };
 
+const amountHandler = (e)=>{
+  if (e.target.id === 'amount_one') {
+    amountTwo.value = fixedHandler(e.target.value * currencyRate[toCurrency]);
+  } 
+  if (e.target.id === 'amount_two') {
+    amountOne.value = Math.floor(e.target.value / currencyRate[toCurrency]);
+  }
+}
+
+
 window.addEventListener("DOMContentLoaded", init);
+amountOne.addEventListener('input',amountHandler)
+amountTwo.addEventListener('input',amountHandler)
