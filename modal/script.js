3ee2e91a41f90a4251805be89d7b6624eca241ab
송승찬 <superfly09@daum.net>
2022-12-31 14:49:00 +0900

    feat: 모달/네비게이션 show/hide 스크립트 추가

---
+++ modal/script.js
@@ -0,0 +1,18 @@
+const qs = (target, scope = document) => scope.querySelector(target);
+
+const modalCloseBtn = qs('#close');
+const body = qs('body');
+const modalOpenBtn = qs('#open');
+const navToggleBtn = qs('.toggle_nav');
+const modal = qs('#modal');
+const submitBtn = qs('input[type="submit"]');
+
+const toggleModalHandler = ()=>{
+    modal.classList.toggle('show_modal');
+}
+modalOpenBtn.addEventListener('click', toggleModalHandler);
+modalCloseBtn.addEventListener('click', toggleModalHandler);
+submitBtn.addEventListener('click',toggleModalHandler);
+navToggleBtn.addEventListener('click', ()=>{
+    body.classList.toggle('show_nav');
+})
