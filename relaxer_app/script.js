301074442523e2fb63ddb9e9e22389670799371f
송승찬 <superfly09@daum.net>
2023-01-08 19:49:31 +0900

    feat: 애니메이션 마무리 - 공 rotate / container sclae 효과 추가

--- relaxer_app/script.js
+++ relaxer_app/script.js
@@ -1,29 +1,22 @@
 import { qs } from '../util.js';
 const totalTime = 7500; //ms
 const breatheInTime = (totalTime / 5 ) * 2
-const holdTome = (totalTime / 5) * 1;
+const holdTome = totalTime / 5;
 const text = qs('.text');
 const container = qs('.container');
-
-// 텍스트 처리 + 애니메이션 (클래스 값)
-// breatheIn  
-// 그후 hold
-// breathOut
 
 const getBreath = ()=>{
     console.log('check')
     text.innerText = 'Breathe In';
-    container.classList.add('in');
-    let holdTimeInterval,outTimeInterval;
-
+    container.className='container grow';
     
-    holdTimeInterval = setInterval(()=>{
+    const holdTimeInterval = setInterval(()=>{
         text.innerText = 'Hold'
-        container.classList.replace('in','hold');
         clearInterval(holdTimeInterval);
-        outTimeInterval = setInterval(()=>{
+
+        const outTimeInterval = setInterval(()=>{
             text.innerText = 'Breath Out'
-            container.classList.replace('hold','out');
+            container.className='container shrink';
             clearInterval(outTimeInterval);
         },holdTome)
     },breatheInTime)
